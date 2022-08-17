2dfa8cdedbacb71a23a5597fab81d7b0c51e88e0
Ryan Dey <techrpd15113@gmail.com>
2022-08-17 14:07:33 -0400

    volunteerModel schema added
    Needs verification method still

---
+++ models/volunteerModel.js
@@ -0,0 +1,85 @@
+const mongoose = require("mongoose");
+
+const volunteerSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  streetAddress: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zipCode: {
+    type: String,
+    required: true,
+  },
+  phoneNumberHome: {
+    type: String,
+    required: false,
+  },
+  phoneNumberCell: {
+    type: String,
+    required: false,
+  },
+  phoneNumberWork: {
+    type: String,
+    required: false,
+  },
+  // may need to be added to another schema
+  education: {
+    type: String,
+    required: true,
+  },
+  emergencyContactName: {
+    type: String,
+    required: true,
+  },
+  emergencyContactPhone: {
+    type: String,
+    required: true,
+  },
+  emergencyContactEmail: {
+    type: String,
+    required: true,
+  },
+  hasCopyOfID: {
+    type: Boolean,
+    required: true,
+  },
+  hasCopyOfSSN: {
+    type: Boolean,
+    required: true,
+  },
+  // may need to be added to another schema
+  approvalStatus: {
+    type: String,
+    required: true,
+  },
+  // NEED availability times, current licenses, skills/interests, and preferred centers
+});
