24e96bc06435620be26e6745729b1a7fa19ccf08
Ryan Dey <ryandeyp@gmali.com>
2022-08-19 12:33:00 -0400

    Added navigation to home/volunteers/opportunities

--- frontend/src/Pages/home.js
+++ frontend/src/Pages/home.js
@@ -1,36 +1,7 @@
-import { useEffect } from 'react'
-import { useVolunteersContext } from '../hooks/useVolunteersContext'
-
-// components
-import VolunteerDetails from '../components/VolunteerDetails'
-
 const Home = () => {
-    const { volunteers, dispatch } = useVolunteersContext();
-
-    useEffect(() => {
-        const fetchVolunteers = async () => {
-            const response = await fetch('/api/volunteers');
-            const json = await response.json(); 
-
-            if (response.ok) {
-                dispatch({ type: 'SET_VOLUNTEERS', payload: json })
-            }
-        };
-        fetchVolunteers();
-    }, [dispatch]);
-
     return(
         <div className="home">
-            {/* <div className='volunteers'>
-                {volunteers && volunteers.map((volunteer) => (
-                    <p key={volunteer._id}>{volunteer.username}</p>
-                ))}
-            </div> */}
-            <div className="vol-list">
-                {volunteers && volunteers.map((volunteer) => (
-                    <VolunteerDetails key={volunteer._id} volunteer={volunteer} />
-                ))}
-            </div>
+            Home page
         </div>
     )
 }
