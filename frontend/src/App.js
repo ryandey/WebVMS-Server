0702a7a937f64c30fd3642c97a6c6b9621475147
Ryan Dey <ryandeyp@gmali.com>
2022-08-19 12:30:45 -0400

    Add routes for navigating to home/volunteers/opportunities pages
    Moved volunteer list to its respective page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './Pages/home';
 import Navbar from './components/Navbar';
+import Volunteers from './Pages/Volunteers';
+import Opportunities from './Pages/Opportunities';
 
 function App() {
   return (
@@ -9,9 +11,9 @@ function App() {
       <Navbar />
       <div className = "pages">
         <Routes>
-          <Route path="/"
-          element={<Home />}
-          />
+          <Route path="/" element={<Home />}/>
+          <Route path="/volunteers" element={<Volunteers />} />
+          <Route path="/opportunities" element={<Opportunities />} />
         </Routes>
       </div>
       </BrowserRouter>
---
+++ frontend/src/Pages/Volunteers.js
@@ -0,0 +1,44 @@
+import { useEffect } from 'react'
+import { useVolunteersContext } from '../hooks/useVolunteersContext'
+
+// components
+import VolunteerDetails from '../components/VolunteerDetails'
+
+const Volunteers = () => {
+  const { volunteers, dispatch } = useVolunteersContext();
+
+    useEffect(() => {
+      const fetchVolunteers = async () => {
+        const response = await fetch('/api/volunteers');
+        const json = await response.json(); 
+
+        if (response.ok) {
+            dispatch({ type: 'SET_VOLUNTEERS', payload: json })
+        }
+    };
+      fetchVolunteers();
+    }, [dispatch]);
+
+  return (
+    <div className="home">
+      <div className="menu">
+        <h2>Manage Volunteers</h2>
+        <div className="menu-buttons">
+          <button className="btnAdd">Add <span className="material-symbols-outlined">add</span></button>
+        </div>
+      </div>
+      {/* <div className='volunteers'>
+          {volunteers && volunteers.map((volunteer) => (
+              <p key={volunteer._id}>{volunteer.username}</p>
+          ))}
+      </div> */}
+      <div className="vol-list">
+        {volunteers && volunteers.map((volunteer) => (
+          <VolunteerDetails key={volunteer._id} volunteer={volunteer} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Volunteers
