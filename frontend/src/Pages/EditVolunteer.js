88da5b28163fa27ad3e610fa4ac6a68845841222
jeblax <jakeburns1291999@gmail.com>
2022-08-20 15:31:55 -0400

    asd

--- frontend/src/Pages/EditVolunteer.js
+++ frontend/src/Pages/EditVolunteer.js
@@ -1,12 +1,14 @@
 import React from 'react'
-
+import { useVolunteersContext } from '../hooks/useVolunteersContext'
+import { Volunteers } from './Volunteers' 
 // components
 import EditVolunteerForm from '../components/EditVolunteerForm'
 
 const EditVolunteer = () => {
   return (
     <div>
-      <EditVolunteerForm />
+      <EditVolunteerForm key={volunteer._id} volunteer={volunteer}/>
+      
     </div>
   )
 }
