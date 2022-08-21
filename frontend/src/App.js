7e0a5c9131af8be7ac0f039ae234282d9dc0b90d
Parker Miller <n01451124@unf.edu>
2022-08-20 21:29:47 -0400

    Added some filters

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './Pages/home';
 import Navbar from './components/Navbar';
 import Volunteers from './Pages/Volunteers';
+import PendingVolunteers from './Pages/PendingVolunteers'
 import AddVolunteer from './Pages/AddVolunteer';
 import Opportunities from './Pages/Opportunities';
 import EditVolunteer from './Pages/EditVolunteer';
@@ -16,6 +17,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/volunteers" element={<Volunteers />} />
+          <Route path="/volunteers/pending" element={<PendingVolunteers />} />
+          <Route path="/volunteers/approved" element={<ApprovedVolunteers />} />
+          <Route path="/volunteers/disapproved" element={<DisapprovedVolunteers />} />
+          <Route path="/volunteers/mixed" element={<MixedVolunteers />} />
           <Route path="volunteers/add" element={<AddVolunteer />} />
           <Route path="volunteers/edit" element={<EditVolunteer />}/>
           <Route path="/opportunities" element={<Opportunities />} />
---
+++ frontend/src/Pages/DisapprovedVolunteers.js
@@ -0,0 +1,52 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import { useVolunteersContext } from '../hooks/useVolunteersContext'
+
+// components
+import VolunteerDetails from '../components/VolunteerDetails'
+import EditVolunteerForm from '../components/EditVolunteerForm';
+
+const Volunteers = () => {
+  const { volunteers, dispatch } = useVolunteersContext();
+
+    useEffect(() => {
+      const fetchVolunteers = async () => {
+        const response = await fetch('/api/volunteers/disapproved');
+        const json = await response.json(); 
+
+        if (response.ok) {
+            dispatch({ type: 'SET_VOLUNTEERS', payload: json })
+        }
+    };
+      fetchVolunteers();
+    }, [dispatch]);
+
+  return (
+    <div className="volunteers">
+      <div className="menu">
+        <h2>Manage Volunteers</h2>
+        <div className="menu-buttons">
+          <form action="search">
+            <input type="text" placeholder='Search volunteers'/>
+          </form>
+          <Link to="/volunteers/add">
+            <button className="btnAdd">Add <span className="material-symbols-outlined">add</span></button>
+          </Link>
+        </div>
+      </div>
+      {/* <div className='volunteers'>
+          {volunteers && volunteers.map((volunteer) => (
+              <p key={volunteer._id}>{volunteer.username}</p>
+          ))}
+      </div> */}
+      <div className="vol-list">
+        {volunteers && volunteers.map((volunteer) => (
+          <VolunteerDetails key={volunteer._id} volunteer={volunteer} />
+        ))}
+        
+      </div>
+    </div>
+  )
+}
+
+export default Volunteers
